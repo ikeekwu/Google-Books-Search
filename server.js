2dc300ad7af70f2335a065e13873defe066413ec
Ike Ekwueme <cryptoekwu@gmail.com>
2020-06-11 19:03:24 -0500

    The files I meant to upload

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const path = require("path");
+const mongoose = require("mongoose")
 const PORT = process.env.PORT || 3001;
+const routes = ("./routes");
 const app = express();
 
 // Define middleware here
@@ -12,12 +14,15 @@ if (process.env.NODE_ENV === "production") {
 }
 
 // Define API routes here
+app.use(routes);
 
 // Send every other request to the React app
 // Define any API routes before this runs
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/googlebooks", {useNewUrlParser: true});
 
 app.listen(PORT, () => {
   console.log(`🌎 ==> API server now on port ${PORT}!`);
